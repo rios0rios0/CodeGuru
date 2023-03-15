3b485550692f611c6641a6952551bfabfadd924e
Felipe Rios <felipe.rios.silva@outlook.com>
2023-03-15 14:10:11 -0300

    hotfix(openai): changed to the most recent OpenAI library

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"os"
 
-	openai "github.com/sashabaranov/go-openai"
-	gitlab "github.com/xanzy/go-gitlab"
+	"github.com/sashabaranov/go-openai"
+	"github.com/xanzy/go-gitlab"
 )
 
 func main() {
 	gitlabToken := os.Getenv("GITLAB_API_TOKEN")
+	gitlabProjectID := os.Getenv("GITLAB_PROJECT_ID")
 	openaiKey := os.Getenv("OPENAI_API_KEY")
 
 	if gitlabToken == "" || openaiKey == "" {
@@ -19,18 +20,16 @@ func main() {
 	}
 
 	git, _ := gitlab.NewClient(gitlabToken)
-	openaiClient := openai.NewAPIClient(openai.NewConfiguration().WithAPIKey(openaiKey))
+	openaiClient := openai.NewClient(openaiKey)
 
-	projectID := "your-gitlab-project-id"
-
-	mergeRequests, _, err := git.MergeRequests.ListProjectMergeRequests(projectID, &gitlab.ListProjectMergeRequestsOptions{})
+	mergeRequests, _, err := git.MergeRequests.ListProjectMergeRequests(gitlabProjectID, &gitlab.ListProjectMergeRequestsOptions{})
 	if err != nil {
 		fmt.Printf("Error fetching merge requests: %v\n", err)
 		return
 	}
 
 	for _, mr := range mergeRequests {
-		changes, _, err := git.MergeRequests.GetMergeRequestChanges(projectID, mr.IID, nil)
+		changes, _, err := git.MergeRequests.GetMergeRequestChanges(gitlabProjectID, mr.IID, nil)
 		if err != nil {
 			fmt.Printf("Error fetching merge request changes: %v\n", err)
 			continue
@@ -44,24 +43,25 @@ func main() {
 			code += change.Diff
 		}
 
-		ctx := context.Background()
 		prompt := fmt.Sprintf("Review the following code changes in Go:\n\n%s", code)
-		result, _, err := openaiClient.CompletionsApi.CreateCompletion(ctx, "text-davinci-codex-002", openai.Completion{
-			Prompt: &prompt,
-		})
+		completions, err := openaiClient.CreateCompletion(context.Background(),
+			openai.CompletionRequest{
+				Prompt: prompt,
+				Model:  "text-davinci-codex-002",
+			})
 		if err != nil {
 			fmt.Printf("Error generating code review: %v\n", err)
 			continue
 		}
 
-		reviewComment := fmt.Sprintf("Code review by ChatGPT:\n%s", *result.Choices[0].Text)
+		reviewComment := fmt.Sprintf("Code review by ChatGPT:\n%s", completions.Choices[0].Text)
 		fmt.Printf("Review for merge request #%d:\n%s\n", mr.IID, reviewComment)
 
 		// Post the review as a comment on the merge request
 		comment := &gitlab.CreateMergeRequestNoteOptions{
 			Body: &reviewComment,
 		}
-		_, _, err = git.Notes.CreateMergeRequestNote(projectID, mr.IID, comment)
+		_, _, err = git.Notes.CreateMergeRequestNote(gitlabProjectID, mr.IID, comment)
 		if err != nil {
 			fmt.Printf("Error posting review comment: %v\n", err)
 		}
